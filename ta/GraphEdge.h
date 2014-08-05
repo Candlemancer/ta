53b1dc8943052389d162ad2dbdbfd2f9a9dd6664
Paul Baker <paul.n.baker@utah.edu>
2014-08-05 16:53:27 -0600

    Implmeneted a graph edge.

---
+++ ta/GraphEdge.h
@@ -0,0 +1,28 @@
+#import "GraphNode.h"
+
+template <class Edge_Type> class GraphEdge
+{
+public:
+    GraphEdge(GraphNode<Edge_Type> &adjacent_node, int node_weight);
+    GraphNode<Edge_Type> getAdjacentNode();
+    int getWeight();
+private:
+    GraphNode<Edge_Type> node;
+    int weight;
+};
+
+template <class Edge_Type> GraphEdge<Edge_Type>::GraphEdge(Edge_Type &adjacent_node, int node_weight)
+{
+    node = adjacent_node;
+    weight = node_weight;
+}
+
+template <class Edge_Type> GraphNode<Edge_Type> GraphEdge<Edge_Type>::getAdjacentNode()
+{
+    return node;
+}
+
+template <class Edge_Type> int GraphEdge<Edge_Type>::getWeight()
+{
+    return weight;
+}
--- ta/GraphNode.h
+++ ta/GraphNode.h
@@ -1,40 +1,40 @@
-template <class T> class GraphNode
+template <class Node_Type> class GraphNode
 {
 public:
-    GraphNode(T &nodeElement);
+    GraphNode(Node_Type &node_element);
     int getInDegree();
     void incrementInDegree();
     bool getVisited();
     void setVisited();
 private:
-    T element;
+    Node_Type element;
     bool isVisited;
     int inDegree;
 };
 
-template <class T> GraphNode<T>::GraphNode(T &nodeElement)
+template <class Node_Type> GraphNode<Node_Type>::GraphNode(Node_Type &node_element)
 {
-    element = nodeElement;
+    element = node_element;
     inDegree = 0;
     isVisited = false;
 }
 
-template <class T> int GraphNode<T>::getInDegree()
+template <class Node_Type> int GraphNode<Node_Type>::getInDegree()
 {
     return inDegree;
 }
 
-template <class T> void GraphNode<T>::incrementInDegree()
+template <class Node_Type> void GraphNode<Node_Type>::incrementInDegree()
 {
     inDegree += 1;
 }
 
-template <class T> bool GraphNode<T>::getVisited()
+template <class Node_Type> bool GraphNode<Node_Type>::getVisited()
 {
     return isVisited;
 }
 
-template <class T> void GraphNode<T>::setVisited()
+template <class Node_Type> void GraphNode<Node_Type>::setVisited()
 {
     isVisited = true;
 }
