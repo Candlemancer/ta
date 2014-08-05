f57fc2be70855d0ee9e442b81dd619689ef598c1
Paul Baker <paul.n.baker@utah.edu>
2014-08-05 14:49:08 -0600

    Fixed compile script. Compiles and then links the files.

--- ta/compile.sh
+++ ta/compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 clear && clear
-g++ -std=c++0x -o ta topological-apt.cpp -lapt-pkg #-laptlib -L/usr/lib
+#g++ -std=c++0x -o ta topological-apt.cpp -lapt-pkg
+g++ -std=c++0x -Wall -g -c topological-apt.cpp #-lapt-pkg
+g++ -std=c++0x -Wall -g -o ta topological-apt.o -lapt-pkg
