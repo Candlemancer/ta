9ef3e672e0bc2eb6bce9c3b414397c5f0bf6e18a
Paul Baker <paul.n.baker@utah.edu>
2014-08-05 16:58:32 -0600

    Updated compile script to only link if compile succeeded

--- ta/compile.sh
+++ ta/compile.sh
@@ -2,5 +2,5 @@
 ./clean.sh
 clear && clear
 #g++ -std=c++0x -o ta topological-apt.cpp -lapt-pkg
-g++ -std=c++11 -Wall -g -c topological-apt.cpp #-lapt-pkg
+g++ -std=c++11 -Wall -g -c topological-apt.cpp && \
 g++ -std=c++11 -Wall -g -o ta topological-apt.o -lapt-pkg
