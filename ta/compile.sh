7128cf1c45e9234c8abbfca193f9395247efe3ff
Paul Baker <paul.n.baker@utah.edu>
2014-08-08 15:47:26 -0600

    Removing un-neccessary files

--- ta/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./clean.sh
-clear && clear
-#g++ -std=c++0x -o ta topological-apt.cpp -lapt-pkg
-g++ -std=c++11 -Wall -g -c topological-apt.cpp && \
-g++ -std=c++11 -Wall -g -o ta topological-apt.o -lapt-pkg
