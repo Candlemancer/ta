4fc522539762196ce8c66d1a2ccb8dcc536e4b1d
Paul Baker <paul.n.baker@utah.edu>
2014-08-08 13:58:18 -0600

    Getting really close now, I think I almost have this...

--- graph.h
+++ graph.h
@@ -2,12 +2,17 @@
 #define GRAPH_H
 
 #include <string>
+#include <unordered_map>
 #include "graphnode.h"
 #include "graphedge.h"
 using namespace std;
+class GraphNode;
+class GraphEdge;
 
 class Graph
 {
+private:
+    unordered_map<string, GraphNode> nodeMap;
 public:
     Graph();
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 //#include <QCoreApplication>
 #include <QDebug>
-#include "Graph.h"
+#include "graph.h"
 
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
     //return a.exec();
     qDebug() << "Hello Bash!" << endl << endl;
+    Graph test;
     return 0;
 }
